fe0fde728ff1fe15ac5da7bf22ce65a5c780c8af
Mustafa-Eldeeb <darshm466@gmail.com>
2020-11-16 23:36:00 +0200

    Return the aggregate CPU utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -8,10 +8,17 @@ using std::array;
 // to prevent divide by zero , initialize prev_cpu_jiffies for the first time.
 Processor::Processor()
 {
-
+    GetLinuxParserForJiffies(prev_cpu_jiffies_);
 }
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization()
+{
+    GetLinuxParserForJiffies(prev_cpu_jiffies_);
+    CalculateDeltaTime(current_cpu_jiffies_,prev_cpu_jiffies_);
+    const float utilization = CalculateUtilization();
+    current_cpu_jiffies_.swap(prev_cpu_jiffies_);
+    return utilization;
+}
 
  void Processor::CalculateDeltaTime(std::array<long,3>&first_cpu_jiffies,std::array<long,3>&second_cpu_jiffies)
  {
@@ -28,3 +35,16 @@ void Processor::GetLinuxParserForJiffies(std::array<long,3>& cpu_jiffies)
     cpu_jiffies[LinuxParser::CPUTime::kTotalTime_]=LinuxParser::Jiffies();   
 }
 
+
+long Processor::getTotalJiffies()
+{
+    return current_cpu_jiffies_[LinuxParser::CPUTime::kTotalTime_];
+}
+
+void Processor::UpdatePrevJiffies()
+{
+    for(size_t i =0; i<prev_cpu_jiffies_.size(); ++i)
+    {
+        prev_cpu_jiffies_[i]=current_cpu_jiffies_[i];
+    }
+}
