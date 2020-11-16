453f04062fca562e9f3d1262a54e8bc76466f228
Mustafa-Eldeeb <darshm466@gmail.com>
2020-11-16 22:15:03 +0200

    Declare necessary private members

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,25 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+
+#include <array>
+#include <vector>
+
 class Processor {
  public:
+  Processor(); //contructor
   float Utilization();  // TODO: See src/processor.cpp
-
+  
+  long getTotalJiffies();
   // TODO: Declare any necessary private members
  private:
+ void CalculateDeltaTime(std::array<long,4>&,std::array<long,3>&);
+ virtual void GetLinuxParserForJiffies(std::array<long,3>&);
+ float CalculateUtilization();
+ void UpdatePrevJiffies();
+ std::array<long,3> current_cpu_jiffies_;
+ std::array<long,3> prev_cpu_jiffies_;
+ std::array<float,3> delta_jiffies_;
 };
 
 #endif
